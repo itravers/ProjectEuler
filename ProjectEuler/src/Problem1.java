9bf383176c990e913968857c456909fe3046474e
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-08-30 21:35:13 -0700

    fixed problem with or operator in project 1

--- ProjectEuler/src/Problem1.java
+++ ProjectEuler/src/Problem1.java
@@ -18,7 +18,7 @@ public class Problem1 {
 	}
 
 	private static boolean isMultipleOf3Or5(int i) {
-		if(i % 3 == 0 | i % 5 == 0){
+		if(i % 3 == 0 || i % 5 == 0){
 			return true;
 		}else{
 			return false;

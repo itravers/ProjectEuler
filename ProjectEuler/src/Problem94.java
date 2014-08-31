6c61c5ad2404a9cef8e6ce49cb83606167036cb7
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-08-31 02:08:14 -0700

    solved 94, whoh

--- ProjectEuler/src/Problem94.java
+++ ProjectEuler/src/Problem94.java
@@ -12,11 +12,57 @@
  */
 public class Problem94 {
 	public static void main(String[] args){
-		System.out.println(getArea(5, 5, 6));
+		long x = 2;
+		long y = 1;
+		long limit = 1000000000;
+		long result = 0;
+		 
+		while(true){
+		    // b = a+1
+		    long aTimes3 = 2 * x - 1;
+		    long areaTimes3 = y * (x - 2);
+		    if (aTimes3 > limit) break;
+		 
+		    if (aTimes3 > 0 &&
+		        areaTimes3 > 0 &&
+		        aTimes3 % 3 == 0 &&
+		        areaTimes3 % 3 == 0) {
+		 
+		        long a = aTimes3 / 3;
+		        long area = areaTimes3 / 3;
+		 
+		        result += 3 * a + 1;
+		    }
+		 
+		    //b = a-1
+		    aTimes3 = 2 * x + 1;
+		    areaTimes3 = y * (x + 2);
+		 
+		    if (aTimes3 > 0 &&
+		        areaTimes3 > 0 &&
+		        aTimes3 % 3 == 0 &&
+		        areaTimes3 % 3 == 0) {
+		 
+		        long a = aTimes3 / 3;
+		        long area = areaTimes3 / 3;
+		 
+		        result += 3 * a - 1;
+		    }
+		 
+		    long nextx = 2 * x + y * 3;
+		    long nexty = y * 2 + x;
+		 
+		    x = nextx;
+		    y = nexty;
+		}
+		System.out.println("Answer: " + result);
 	}
 	
-	public static double getArea(int a, int b, int c){
-		double s = (a+b+c)/2;
+	
+	
+	public static double getArea(long a, long b, long c){
+		double s = (double) ((a+b+c)/2);
+		
 		double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
 		return area;
 	}

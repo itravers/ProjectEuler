e31ba15bdc430010b7d9463cacd5bc4621fa9a54
Isaac Assegai <confusedvirtuoso@gmail.com>
2014-09-01 19:40:26 -0700

    first attempt at problem 9

--- ProjectEuler/src/Problem9.java
+++ ProjectEuler/src/Problem9.java
@@ -10,6 +10,22 @@
  */
 public class Problem9 {
 	public static void main(String [] args){
-		
+		int goalNum = 1000;
+		long product = findPythagoreanTripletProduct(goalNum);
+		System.out.println("Answer: " + product);
+	}
+
+	private static long findPythagoreanTripletProduct(int goalNum) {
+		long product = 0;
+		for(int a = 0; a <= 1000; a++){
+			for(int b = (a+1); b <= goalNum; b++){
+				for(int c = (b+1); c <= goalNum; c++){
+					if(((a+b+c)) == goalNum  && ((a*a)+(b*b) == (c*c))){
+						product = a*b*c;
+					}
+				}
+			}
+		}
+		return product;
 	}
 }
